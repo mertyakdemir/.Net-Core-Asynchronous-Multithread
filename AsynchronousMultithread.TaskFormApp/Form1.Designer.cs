6f73d65a1148fca71326a80a8430b97d1bd2d4e8
mertyakdemir <mertyakdemir07@gmail.com>
2021-03-14 20:26:44 +0300

    Add project files.

---
+++ AsynchronousMultithread.TaskFormApp/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace AsynchronousMultithread.TaskFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnReadFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // BtnReadFile
+            // 
+            this.BtnReadFile.Location = new System.Drawing.Point(124, 47);
+            this.BtnReadFile.Name = "BtnReadFile";
+            this.BtnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnReadFile.TabIndex = 0;
+            this.BtnReadFile.Text = "Read";
+            this.BtnReadFile.UseVisualStyleBackColor = true;
+            this.BtnReadFile.Click += new System.EventHandler(this.BtnReadFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(124, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnCounter
+            // 
+            this.BtnCounter.Location = new System.Drawing.Point(378, 47);
+            this.BtnCounter.Name = "BtnCounter";
+            this.BtnCounter.Size = new System.Drawing.Size(75, 23);
+            this.BtnCounter.TabIndex = 2;
+            this.BtnCounter.Text = "Counter";
+            this.BtnCounter.UseVisualStyleBackColor = true;
+            this.BtnCounter.Click += new System.EventHandler(this.BtnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(378, 87);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(631, 87);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.BtnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnReadFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ AsynchronousMultithread.TaskFormApp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsynchronousMultithread.TaskFormApp
+{
+    public partial class Form1 : Form
+    {
+
+        public int counter { get; set; } = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void BtnReadFile_Click(object sender, EventArgs e)
+        {
+            // string data = ReadFile();
+
+            string data = string.Empty;
+            Task<string> read = ReadFileAsync();
+
+            richTextBox2.Text = await new HttpClient().GetStringAsync("https://www.youtube.com/");
+
+            data = await read;
+
+            richTextBox1.Text = data;
+        }
+
+        private void BtnCounter_Click(object sender, EventArgs e)
+        {
+            textBoxCounter.Text = counter++.ToString();
+        }
+
+        private string ReadFile()
+        {
+            string data = string.Empty;
+            using(StreamReader s = new  StreamReader("names.txt"))
+            {
+                Thread.Sleep(5000);
+                data = s.ReadToEnd();
+            }
+
+            return data;
+        }
+
+        private async Task<string> ReadFileAsync()
+        {
+            string data = string.Empty;
+            using (StreamReader s = new StreamReader("names.txt"))
+            {
+                Task<string> myTask = s.ReadToEndAsync();
+
+
+                data = await myTask;
+
+                return data;
+            }
+        }
+
+    }
+}
